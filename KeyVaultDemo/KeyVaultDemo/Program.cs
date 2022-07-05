b28760ac913c33db79acba930985bc4a9d41af65
ronaldmariah <ronaldmariah@gmail.com>
2022-07-06 00:16:35 +0200

    Nuget package update.

--- KeyVaultDemo/KeyVaultDemo/Program.cs
+++ KeyVaultDemo/KeyVaultDemo/Program.cs
@@ -1,9 +1,6 @@
 ﻿using Azure.Identity;
-using Azure.Security.KeyVault.Keys;
 using Azure.Security.KeyVault.Secrets;
-using Microsoft.Azure.KeyVault;
 using Microsoft.Azure.Services.AppAuthentication;
-using Microsoft.Rest;
 
 namespace KeyVaultDemo
 {
@@ -11,7 +8,6 @@ namespace KeyVaultDemo
     {
         static async Task Main(string[] args)
         {
-            var azureServiceTokenProvider = new AzureServiceTokenProvider();
             var vaultUrl = "https://rm-keyvault-demo.vault.azure.net";
             var client = new SecretClient(vaultUri: new Uri(vaultUrl), credential: new DefaultAzureCredential());
 

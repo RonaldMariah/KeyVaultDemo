5b710ba83cf6e60e736cf999d042733e1df87c49
ronaldmariah <ronaldmariah@gmail.com>
2022-07-06 00:07:21 +0200

    KeyVaultDemo.

---
+++ KeyVaultDemo/KeyVaultDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Keys;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Rest;
+
+namespace KeyVaultDemo
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var azureServiceTokenProvider = new AzureServiceTokenProvider();
+            var vaultUrl = "https://rm-keyvault-demo.vault.azure.net";
+            var client = new SecretClient(vaultUri: new Uri(vaultUrl), credential: new DefaultAzureCredential());
+
+            // Retrieve a key using the key client.
+            var secret = (await client.GetSecretAsync("Flintstone")).Value.Value;
+
+            Console.WriteLine(secret);
+            Console.ReadKey();
+        }
+    }
+}
